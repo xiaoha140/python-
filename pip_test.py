9eaff78f524526796b120f2f6bb844ccbea0f6c7
xiaoha140 <107303961+xiaoha140@users.noreply.github.com>
2022-06-12 01:19:28 +0800

    Create pip_test.py
    python安装后pip无法使用的问题

---
+++ pip_test.py
@@ -0,0 +1,9 @@
+电脑上的Python为3.5，安装在Windows上，虽然安装过程中选择了pip，但是在命令行输入pip后仍然不能成功，尝试一下方法，终于解决问题
+
+//1、使用下面的语句确保没有报错了
+python -m ensurepip
+//2、使用pip -V命令查看，依然提示没有，继续输入如下命令测试是否可用
+python -m pip install readline 
+//3.怀疑可能是环境没配置好，所以更新一下pip试试
+python -m pip install --upgrade pip
+//再输入pip，就正常了
